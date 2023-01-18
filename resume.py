726bd8af94bc9c361daebfef52bd3ea7e839e73b
Basudev Tyagi <tyagi101basudev@gmail.com>
2023-01-18 16:14:20 +0530

    feat: logger for acknowledgement

--- resume.py
+++ resume.py
@@ -1,12 +1,20 @@
 import os
 import requests
 import json
+from datetime import datetime
 from dotenv import load_dotenv
 
+
+def log(message):
+    print(f"[RESUME] {datetime.now()} - {message}")
+
+
+log("Loading environment variables...")
 load_dotenv()
 
 url = "https://hackerresume.com/api/generate/resume"
 
+log("Loading JSON data...")
 resume_data = json.load(open(os.path.abspath(".")+"/resume-data.json", 'r', encoding='utf-8'))
 
 resume_data['selectedTemplate'] = os.getenv('SELECTED_TEMPLATE')
@@ -16,17 +24,21 @@ resume_data['user']['token'] = os.getenv('HACKERRANK_TOKEN')
 resume_data['resumeId'] = os.getenv('RESUME_ID')
 
 
+log("Generating Resume...")
 response = requests.request(
     "POST",
     url,
     headers={
     'Content-Type': 'application/json'
 },
-data=json.dumps(resume_data))
+data=json.dumps(resume_data)
+)
 
 if response.status_code != 200:
-    print(response.text)
+    log(f"Error: {response.text}")
     exit()
 
+log(f"Writing new resume...")
 with open(os.path.abspath(".")+"/resources/Basudev Tyagi.pdf", 'wb') as file:
     file.write(response.content)
+log(f"SUCCESS")
